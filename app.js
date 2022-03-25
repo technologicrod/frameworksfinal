33784277e5de071674fd36c168cdc5eb6f95f66e
technologicrod <matchon.rodwell@gmail.com>
2022-03-25 21:02:21 +0800

    added heroku site to interval

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 
 var http = require("http");
 setInterval(function() {
-    http.get("http://<your app name>.herokuapp.com");
+    http.get("https://desolate-peak-93013.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
 
 global.tally = [
